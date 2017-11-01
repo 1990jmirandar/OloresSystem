97f3d0263ffc483933e738c07fe8df8e76b51235
Neycker Aguayo <neyaldrichdev@outlook.com>
2017-11-01 08:53:07 -0500

    Avance

---
+++ src/main/java/com/sinergiass/models/Olor.java
@@ -0,0 +1,23 @@
+package com.sinergiass.models;
+
+public class Olor {
+	
+	private String hexColor;
+	private String name;
+	
+	public String getHexColor() {
+		return hexColor;
+	}
+	public void setHexColor(String hexColor) {
+		this.hexColor = hexColor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
